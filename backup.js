6190ba10100dd235644acd9ba1a23e90d797547f
azrilpramudia <azrilpramudia01@gmail.com>
2025-07-31 02:55:53 +0700

    Javascript

---
+++ backup.js
@@ -0,0 +1,10 @@
+// Menampilkan Output
+console.log('Hello, World')
+
+alert('hallo')
+
+let nama = "Burhan" // Tipe data string
+const umur = 20 // Tipe data 
+
+console.log(nama)
+console.log(umur)
